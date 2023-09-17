d9dc5361eb25c248eefb3912dcea2dd3b9854c8e
lgafree <lelix.guile77@gmail.com>
2023-09-17 14:43:41 +0800

    Modified FB share logic and added favicon

--- src/components/FBShare.svelte
+++ src/components/FBShare.svelte
@@ -6,7 +6,19 @@
 		// 	method: 'share',
 		// 	href: 'https://example.com' // URL you want to share
 		// })
-		const navUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + window.location.href
+		html2canvas(document.body).then(function (canvas) {
+			// Convert the canvas to a data URL
+			const screenshotDataUrl = canvas.toDataURL('image/png')
+
+			// Set the og:image meta tag with the screenshot URL
+			const ogImageMeta = document.querySelector('meta[property="og:image"]')
+			if (ogImageMeta) {
+				ogImageMeta.setAttribute('content', screenshotDataUrl)
+			}
+		})
+
+		const navUrl =
+			'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(window.location.href)
 		window.open(navUrl, 'mywindow', 'width=350,height=250')
 	}
 </script>
