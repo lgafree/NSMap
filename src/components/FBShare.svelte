1e43bcd929769e7d1673670db1f3c0214d6bffc2
lgafree <lelix.guile77@gmail.com>
2023-09-17 15:07:40 +0800

    Fixed fb sharing og image

--- src/components/FBShare.svelte
+++ src/components/FBShare.svelte
@@ -2,22 +2,6 @@
 	import { Button } from 'flowbite-svelte'
 	import { FacebookSolid } from 'flowbite-svelte-icons'
 	function shareOnFacebook() {
-		// FB.ui({
-		// 	method: 'share',
-		// 	href: 'https://example.com' // URL you want to share
-		// })
-		html2canvas(document.body).then(function (canvas) {
-			// Convert the canvas to a data URL
-			const screenshotDataUrl = canvas.toDataURL('image/png')
-
-			// Set the og:image meta tag with the screenshot URL
-			const ogImageMeta = document.querySelector('meta[property="og:image"]')
-			if (ogImageMeta) {
-				ogImageMeta.setAttribute('content', screenshotDataUrl)
-			}
-			console.log(ogImageMeta)
-		})
-
 		const navUrl =
 			'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(window.location.href)
 		window.open(navUrl, 'mywindow', 'width=350,height=250')
